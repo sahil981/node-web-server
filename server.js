0c508527f6a8ae91521d916a1a069606c11554c4
sahildeepmehra <sahil.mehra@guidingstardigital.com>
2019-03-31 00:44:22 +0530

    Template Engine

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const hbs = require('hbs')
 
 var app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     res.send({
         name: 'Sahil',
         college: 'NIT Hamirpur '
+    });
+});
+
+app.get('/about', (req, res)=> {
+    res.render('about.hbs', {
+        pageTitle : 'Test Enjection',
+        year : new Date().getFullYear()
     });
 });
 
