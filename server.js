4507cbdb358c61f34fe096a723e57516bb72686f
sahildeepmehra <sahil.mehra@guidingstardigital.com>
2019-03-30 21:50:44 +0530

    Basic web server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+var app = express();
+
+app.get('/', (req, res) => {
+    res.send({
+        name: 'Sahil',
+        college: 'NIT Hamirpur '
+    });
+});
+
+
+app.get('/bad', (req, res)=> {
+   res.send({
+       errorMessage: 'Page not found !!'
+   });
+});
+app.listen(3000);
